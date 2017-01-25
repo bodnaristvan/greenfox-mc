6306d6606fef448a8eecd788b46cc6ede46e5a75
Istvan Bodnar <istvan@gawker.com>
2017-01-26 00:20:55 +0100

    test(features/cache): implement test cases for cache.get
    Implemented steps needed for the test to work. Tests gets the cache service from the DI container, which is configured to return the in-memory implementation.

--- src/features/steps/cache.js
+++ src/features/steps/cache.js
@@ -1,25 +1,43 @@
 'use strict';
 
+import { assert } from 'chai';
+
 export default function () {
 
+	this.Before(function () {
+		this.context.cache = this.container.get('cache');
+	});
+
 	this.Given(/^There's no such key as "([^"]*)"$/, function (key, callback) {
-		callback(null, 'pending');
+		callback();
 	});
 
 	this.Given(/^I set cache key "([^"]*)" to "([^"]*)"$/, function (key, value, callback) {
-		callback(null, 'pending');
+		this.context.cache.set(key, value).then(() => {
+			callback();
+		}, callback);
 	});
 
 	this.When(/^I request the key "([^"]*)"$/, function (testKey, callback) {
-		callback(null, 'pending');
+		this.context.cache.get(testKey).then((cacheResponse) => {
+				this.context.cacheResponse = cacheResponse;
+				callback();
+			},
+			(cacheError) => {
+				this.context.cacheError = cacheError;
+				callback();
+			}
+		);
 	});
 
-	this.Then(/^I get a null response$/, function (callback) {
-		callback(null, 'pending');
+	this.Then(/^I get a null response$/, function () {
+		assert.strictEqual(this.context.cacheError, undefined, "Error getting cache key");
+		assert.strictEqual(this.context.cacheResponse, null, "Response not null");
 	});
 
-	this.Then(/^I get a "([^"]*)" response$/, function (response, callback) {
-		callback(null, 'pending');
+	this.Then(/^I get a "([^"]*)" response$/, function (response) {
+		assert.isUndefined(this.context.cacheError, "Error getting cache key");
+		assert.strictEqual(parseInt(this.context.cacheResponse, 10), parseInt(response, 10), "Cache value does not match");
 	});
 
 }
