e77ee3a6b1d28ca9c2014272f09c6e79f8627e6a
Istvan Bodnar <istvan@gawker.com>
2017-01-26 00:26:01 +0100

    feat(services/cache/redis): add redis implementation for cache.get
    Naive implementation, redis connection does not use any configuration at this point, and expects a successful connection. Both the `get` and `set` methods are simple wrappers around the built in redis implementation.

--- src/server/external/cache/implementations/redis.js
+++ src/server/external/cache/implementations/redis.js
@@ -1,5 +1,18 @@
 'use strict'
 
 module.exports = function() {
-	return Object.freeze({});
+
+	const redis = new require('ioredis')();
+
+	const get = async (key) => redis.get(key)
+
+	const set = async (key, value) => {
+		redis.set(key, value)
+		return value
+	}
+
+	return Object.freeze({
+		get,
+		set
+	});
 }
